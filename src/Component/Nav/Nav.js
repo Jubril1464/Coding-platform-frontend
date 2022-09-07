fadf17e74b1d54d199df91ecbaa453b1fa4f1904
Jubril <18-30gd050@students.unilorin.edu.ng>
2022-09-07 14:23:02 +0100

    updated some UI

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -1,79 +1,57 @@
-import React from "react";
+import {useState} from "react";
 import ReactDom from 'react-dom'
 import logo from "../../Assets/logo.png";
-import Button from "../Button/Button";
 import navStyles from './Nav.module.css'
 const domElement = document.getElementById('nav')
 
 
 function Nav() {
+  const [check, setCheck] = useState(false)
+  console.log(check);
+  const handleCheck = () => {
+    setCheck(false)
+
+  }
   return ReactDom.createPortal(
     <header className={navStyles.header}>
-      {/* 
-      <Button
-        color="#fff"
-        backgroundColor="transparent"
-        border="1px solid black"
-        borderRadius="10px"
-      >
-        RSVP
-      </Button> */}
+     
       <div className={navStyles.navigation}>
         <div className={navStyles.logo}>
           <a href="/">
             <img src={logo} alt="" />
           </a>
         </div>
-        {/* <ul className={navStyles.navItems}>
-          <a href="">
-            {" "}
-            <li className={navStyles.navItem}>How we meet</li>{" "}
-          </a>
-          <a href="">
-            {" "}
-            <li className={navStyles.navItem}>Events</li>{" "}
-          </a>
-          <a href="">
-            {" "}
-            <li className={navStyles.navItem}>Instagram</li>{" "}
-          </a>
-          <a href="">
-            {" "}
-            <li className={navStyles.navItem}>Engagement</li>{" "}
-          </a>
-          <a href="">
-            {" "}
-            <li className={navStyles.navItem}>Venue</li>{" "}
-          </a>
-        </ul> */}
+       
         <input
           type="checkbox"
           className={navStyles.navigation__checkbox}
           id="navi-toggle"
+          checked={check}
+          onChange ={(e) => setCheck(e.target.checked)}
         />
-        <label htmlFor="navi-toggle" className={navStyles.navigation__button}>
+        <label htmlFor="navi-toggle" className={navStyles.navigation__button} >
           <span className={navStyles.navigation__icon}>&nbsp;</span>
         </label>
         <div className={navStyles.navigation__background}>&nbsp;</div>
         <nav className={navStyles.navigation__nav}>
           <ul className={navStyles.navigation__list}>
             <li className={navStyles.navigation__item}>
-              <a href="#HowWeMeet" className={navStyles.navigation__link}>
+              <a href="#HowWeMeet" className={navStyles.navigation__link} onClick={handleCheck}>
                 How We Meet
               </a>
             </li>
             <li className={navStyles.navigation__item}>
-              <a href="#event" className={navStyles.navigation__link}>
+              <a href="#event" className={navStyles.navigation__link} onClick={handleCheck}>
                 Events
               </a>
             </li>
             <li className={navStyles.navigation__item}>
-              <a href="#instagram" className={navStyles.navigation__link}>
+              <a href="#instagram" className={navStyles.navigation__link} onClick={handleCheck}>
                 Instagram
               </a>
             </li>
             <li className={navStyles.navigation__item}>
-              <a href="#engagement" className={navStyles.navigation__link}>
+              <a href="#engagement" className={navStyles.navigation__link} onClick={handleCheck}>
                 Engagement
               </a>
             </li>
